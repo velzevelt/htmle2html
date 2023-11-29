040759eb8b7c5c85bb150dafd2789bd7240d2323
Class_Student <Class_Student@rtk.local>
2023-11-29 15:55:31 +0300

    s

--- src/htmle2html.c
+++ src/htmle2html.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <dirent.h>
 
 #if defined(WIN32)
 #include <asprintf.h>
@@ -31,7 +32,6 @@ char *above(const char path[], int above_steps)
     }
     assert(above_steps > 0);
 
-
     int dir_separator_id = -1;
     int above_steps_counter = 1;
     for (int i = path_size - 1; i >= 1; i--)
@@ -42,7 +42,9 @@ char *above(const char path[], int above_steps)
             {
                 dir_separator_id = i;
                 break;
-            } else {
+            }
+            else
+            {
                 above_steps_counter++;
             }
         }
@@ -62,24 +64,80 @@ char *above(const char path[], int above_steps)
     return res;
 }
 
+
+struct dir_info {
+    char **files;
+    size_t length;
+};
+
+// https://stackoverflow.com/questions/4204666/how-to-list-files-in-a-directory-in-a-c-program
+struct dir_info get_dir_files(char *path)
+{
+    DIR *d = opendir(path);
+    if (d == NULL)
+    {
+        perror("No such directory");
+        return;
+    }
+
+    struct dirent *dir;
+    int files_length = 0;
+    while ((dir = readdir(d)) != NULL)
+    {
+        if (dir->d_type == DT_DIR)
+        {
+            continue;
+        }
+        else
+        {
+            files_length++;
+        }
+    }
+    rewinddir(d);
+
+    int i = 0;
+    char *files[files_length];
+    while ((dir = readdir(d)) != NULL)
+    {
+        if (dir->d_type == DT_DIR)
+        {
+            continue;
+        }
+        else
+        {
+            files[i] = (char *)malloc(strlen(dir->d_name) + 1);
+            strncpy(files[i], dir->d_name, strlen(dir->d_name));
+        }
+        i++;
+    }
+    closedir(d);
+
+    struct dir_info res = {files, files_length};
+    return res;
+}
+
 int main(int argc, char **argv)
 {
-    FILE *file;
-    char ch;
-
     char *exe_path = realpath(argv[0], NULL);
     char *exe_dir_path = above(exe_path, 1);
-    char *file_path;
+    struct dir_info test = get_dir_files(exe_dir_path);
 
-    printf("\n%i\n", file_exists(file_path));
-
+    for (int i = 0; i < test.length; i++) {
+        printf("%s\n", test.files[i]);
+    }
     return 0;
 
+    // char *file_path;
+
+    // printf("\n%s %i\n", exe_dir_path, file_exists(exe_dir_path));
+
+    // FILE *file;
+    // char ch;
 
     // file = fopen(file_path, "r");
 
     // if (file == NULL)
-    // {   
+    // {
     //     char *error_message;
     //     asprintf(&error_message, "Can't open file at %s", file_path);
     //     perror(error_message);
