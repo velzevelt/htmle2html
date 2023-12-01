6feb24a0f337fbfefdf637014dedb45dcf88e4f9
velzevelt <novaraptor13@gmail.com>
2023-12-01 22:45:16 +0300

    a

--- src/htmle_interpreter.c
+++ src/htmle_interpreter.c
@@ -32,7 +32,32 @@ const char *interp_htmle(const char input[], const char file_name[])
             char *interp_end = strstr(p_symbol, "?>");
             if (interp_end)
             {
-                char *inter_commands = str
+                char *inter_commands_begin = p_symbol + 3;
+                char *inter_commands_end = interp_end - 1;
+                int size = inter_commands_end - inter_commands_begin + 1;
+                
+                char *command;
+                if (command = strstr(inter_commands_begin, "include"))
+                {
+                    char *arg_begin = strstr(command, "(\"");
+                    char *arg_end = strstr(command, "\")");
+
+                    if (arg_begin && arg_end)
+                    {
+                        printf("We have argument!\n");
+                    }
+                    else
+                    {
+                        char *error_message;
+                        asprintf(&error_message, "ERROR: \'include\' command expects string argument \'path\' e.g. include(\"file_1.txt\") at line %i:%i in file %s\n", line_position + 1, relative_char_position + 1, file_name);
+                        fprintf(stderr, error_message);
+                        strncpy(p_symbol, error_message, strlen(error_message));
+                    }
+
+                }
+
+                // printf("SIZE %i\n", size);
+                // printf("S %c\n", inter_commands_end[0]);
             }
             else
             {
