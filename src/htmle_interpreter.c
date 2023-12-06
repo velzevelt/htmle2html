4939aa03bd4795864b1303ebd38b58d0c4527571
velzevelt <novaraptor13@gmail.com>
2023-12-06 20:37:39 +0300

    a

--- src/htmle_interpreter.c
+++ src/htmle_interpreter.c
@@ -1,24 +1,20 @@
 #include <string.h>
 
-const char *strstr_reverse(const char *haystack, const char *needle)
-{
-    size_t needle_size = strlen(needle);
-    size_t haystack_size = strlen(haystack);
+typedef enum {
+    unknown,
+    comment,
+    new_line,
+    e_begin,
+    e_end,
 
-    for (size_t i = haystack_size; i >= 0; i--)
-    {
-        if (strncmp(&haystack[i], needle, needle_size) == 0)
-        {
-            return &haystack[i];
-        }
-        else if (i == 0)
-        {
-            break;
-        }
-    }
+} token_type;
 
-    return NULL;
-}
+typedef struct {
+    token_type t_type;
+    size_t line_position;
+    char *source;
+    char *out;
+} token_info;
 
 const char *interp_htmle(const char input[], const char file_path[], const dir_info *env)
 {
@@ -37,64 +33,32 @@ const char *interp_htmle(const char input[], const char file_path[], const dir_i
     char input_copy[input_size];
     strncpy(input_copy, input, input_size);
 
-    size_t token_count = 0;
-    char *token = strtok(input_copy, " ");
+    char *token;
+    char *saveptr;
+
+    token = strtok_r(input_copy, " \n", &saveptr);
     while (token)
     {
-        token_count++;
-        token = strtok(NULL, " ");
+        char delimiter = input[token - input_copy + strlen(token)];
+        if (delimiter == '\n')
+        {
+            printf("NEW LINE! %s\n", token);
+        }
+
+        // printf("TOKEN %s\n", token);
+        token = strtok_r(NULL, " \n", &saveptr);
     }
+
+    // token = strtok_r(input_copy, " \n", saveptr);
+    // while (token)
+    // {
+    //     int new_line = strstr(token, "\n");
+    //     printf("TOKEN %s\n", token);
+    //     token = strtok_r(NULL, " \n", saveptr);
+    // }
+
+
     strncpy(input_copy, input, input_size);
-
-    char **tokens = calloc(token_count, sizeof(char*));
-    size_t i = 0;
-    token = strtok(input_copy, " ");
-    while (token)
-    {
-        tokens[i] = calloc(strlen(token) + 1, sizeof(char));
-        strcpy(tokens[i], token);
-
-        token = strtok(NULL, " ");
-        i++;
-    }
-
-    size_t e_begin_position = NULL;
-    size_t e_end_position = NULL;
-    for(size_t j = 0; j < token_count; j++)
-    {
-        // printf("TOKEN: %s\n", tokens[j]);
-
-        if (strncmp(tokens[j], "<?e", 3) == 0 && strncmp(tokens[j - 1], "<!--", 4) != 0)
-        {
-            e_begin_position = j;
-        }
-        else if (strncmp(tokens[j], "?>", 2) == 0 && strncmp(tokens[j - 1], "<!--", 4) != 0)
-        {
-            e_end_position = j;
-        }
-
-        if (e_begin_position && e_end_position)
-        {
-            printf("E begin position %i\n", e_begin_position);
-            printf("E end position %i\n", e_end_position);
-
-            int e_tokens_count = e_end_position - e_begin_position + 1;
-            printf("E count %i\n", e_tokens_count);
-
-            // char **e_tokens = &tokens[e_begin_position];
-            char *e_tokens[e_tokens_count];// = &tokens[e_begin_position];
-            memcpy(e_tokens, &tokens[e_begin_position], e_tokens_count * sizeof(char*));
-
-            for (size_t k = 0; k < e_tokens_count; k++)
-            {
-                printf("E TOKEN %s\n", e_tokens[k]);
-            }
-
-            e_begin_position = NULL;
-            e_end_position = NULL;
-        }
-    }
-
 
     return "123";
 }
