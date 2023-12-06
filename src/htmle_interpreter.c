fed54cbe04970c5f96fe307c4e9ea6ba21d2e50e
Class_Student <Class_Student@rtk.local>
2023-12-06 13:37:52 +0300

    am

--- src/htmle_interpreter.c
+++ src/htmle_interpreter.c
@@ -8,11 +8,27 @@ typedef enum {
 } token_type;
 
 typedef struct {
-    token_type type;
-    char *string;
+    token_type t_type;
+    char *origin;
+    char *out;
     size_t line_postion;
     size_t char_position;
 } htmle_token;
+
+
+const *htmle_token parse_source(const char source[])
+{
+    size_t char_len = strlen(source) + 1;
+    size_t line_len = 0;
+
+    for (size_t i = 0; i <= char_len; i++)
+    {
+        if (source[i] == '\n' || source[i] == '\0')
+        {
+            line_len++;
+        }
+    }
+}
 
 const char *interp_htmle(const char input[], const char file_path[], const dir_info *env)
 {
@@ -28,34 +44,6 @@ const char *interp_htmle(const char input[], const char file_path[], const dir_i
         return NULL;
     }
 
-    char *input_copy = calloc(input_size, sizeof(char));
-    strncpy(input_copy, input, input_size);
 
-    char *e_begin = NULL;
-    char *e_end = NULL;
-    char *html_comment = NULL;
-
-    for (size_t i = 0; i < input_size; i++)
-    {
-        if (strncmp(&input_copy[i], "<!--", 4) == 0)
-        {
-            html_comment = &input_copy[i];
-        }
-        else if (strncmp(&input_copy[i], "<?e", 3) == 0)
-        {
-            e_begin = &input_copy[i];
-        }
-        else if (strncmp(&input_copy[i], "?>", 2) == 0)
-        {
-            e_end = &input_copy[i];
-        }
-
-        if (e_begin != NULL && e_end != NULL)
-        {
-            e_begin = NULL;
-            e_end = NULL;
-        }
-    }
-
-    return input;
+    return "";
 }
