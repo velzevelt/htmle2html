80978a618d7743e48972afed37e0667f1b45274c
Class_Student <Class_Student@rtk.local>
2023-12-06 15:39:27 +0300

    a

--- src/htmle2html.c
+++ src/htmle2html.c
@@ -35,7 +35,7 @@ void compile_rec(int argc, char **argv)
                 // printf("file content:\n%s\n", file_content);
 
                 const char *interp_out = interp_htmle(file_content, file_path, &exe_dir_info);
-                // printf("Interp result: %s\n", interp_out);
+                printf("Interp result: %s\n", interp_out);
                 // printf("Interp result: %s\n", "123");
 
                 if (interp_out != NULL)
--- src/htmle_interpreter.c
+++ src/htmle_interpreter.c
@@ -1,46 +1,5 @@
 #include <string.h>
 
-typedef enum {
-    html_regular,
-    html_comment,
-    htmle_interp_begin,
-    htmle_interp_end,
-} token_type;
-
-typedef struct {
-    token_type t_type;
-    char *origin;
-    char *out;
-    size_t line_postion;
-    size_t char_position;
-} htmle_token;
-
-typedef struct {
-    htmle_token *tokens;
-    size_t token_len;
-    size_t char_len;
-    size_t line_len;
-} ast;
-
-ast parse_source(const char source[])
-{
-    size_t char_len = strlen(source) + 1;
-    size_t line_len = 0;
-
-    for (size_t i = 0; i <= char_len; i++)
-    {
-        if (source[i] == '\n' || source[i] == '\0')
-        {
-            line_len++;
-        }
-    }
-}
-
-const char *compile_ast(const ast *a)
-{
-
-}
-
 const char *interp_htmle(const char input[], const char file_path[], const dir_info *env)
 {
     if (input == NULL)
@@ -55,6 +14,15 @@ const char *interp_htmle(const char input[], const char file_path[], const dir_i
         return NULL;
     }
 
+    char *res = calloc(input_size, sizeof(char));
+    strncpy(res, input, input_size);
+    for (size_t i = 0; i <= input_size; i++)
+    {
+        if (strncmp(&res[i], "<?e", 3) == 0) //&& strstr(get_line_begin(&res[i]))) Respect comments
+        {
+            printf("Found thing\n");
+        }
+    }
 
-    return "";
+    return res;
 }
