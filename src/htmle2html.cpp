a228bba0e1ac0eb4187a3267ae2e7585badd0302
velzevelt <novaraptor13@gmail.com>
2023-12-13 17:23:53 +0300

    a

--- src/htmle2html.cpp
+++ src/htmle2html.cpp
@@ -11,8 +11,6 @@
 #include <shellapi.h>
 #endif
 
-
-
 namespace fs = std::filesystem;
 
 int main(int argc, char **argv)
@@ -24,22 +22,27 @@ int main(int argc, char **argv)
         return EXIT_FAILURE;
     }
 
-
-    #ifdef WIN32
+#ifdef WIN32
     LPWSTR cmd = GetCommandLineW();
     LPWSTR *argvw = CommandLineToArgvW(cmd, &argc);
     std::wstring zero_arg(argvw[1]);
-    #else
+#else
     std::string zero_arg = argv[1];
-    #endif
+#endif
 
+    if (not fs::exists(zero_arg))
+    {
+        std::cerr << "Invalid input provided, directory \"" << zero_arg << "\" not found" << '\n'
+                  << "Usage: htmle2html [directory]" << '\n';
+        return EXIT_FAILURE;
+    }
 
     fs::path arg_path = zero_arg;
     fs::path arg_parent = arg_path.parent_path();
     fs::path absolute_arg_parent = fs::absolute(arg_parent);
 
     std::vector<fs::path> files;
-    for (auto const& dir_entry : fs::recursive_directory_iterator(absolute_arg_parent))
+    for (auto const &dir_entry : fs::recursive_directory_iterator(absolute_arg_parent))
     {
         files.push_back(dir_entry.path());
     }
@@ -50,20 +53,18 @@ int main(int argc, char **argv)
     // }
 
     std::vector<fs::path> htmle_files;
-    std::copy_if(files.begin(), files.end(), std::back_inserter(htmle_files), [](fs::path i) {
-        return i.extension().compare(".htmle") == 0;
-    });
+    std::copy_if(files.begin(), files.end(), std::back_inserter(htmle_files), [](fs::path i)
+                 { return i.extension().compare(".htmle") == 0; });
 
-
-    for (auto const& s : htmle_files)
+    for (auto const &s : htmle_files)
     {
         fs::path new_file_path = s;
         new_file_path.replace_extension(".html");
 
-        std::cout << "Compiling file " << s.filename() 
-                  << " to " << new_file_path.filename() 
+        std::cout << "Compiling file " << s.filename()
+                  << " to " << new_file_path.filename()
                   << '\n';
-        
+
         std::ifstream source_file(s);
         if (!source_file.is_open())
         {
@@ -79,21 +80,55 @@ int main(int argc, char **argv)
             continue;
         }
 
+        size_t line_position = 0;
         std::string line;
         while (std::getline(source_file, line))
         {
+            line_position++;
             std::string out = line;
 
-            auto is_valid_position = [](std::size_t pos) {return pos != std::string::npos;};
+            auto is_valid_position = [](std::size_t pos)
+            { return pos != std::string::npos; };
 
             std::size_t comment_position = line.find("<!--");
-            if (not is_valid_position(comment_position))
-                comment_position = line.find("-->");
+            comment_position = not is_valid_position(comment_position) ? line.find("-->") : comment_position;
+
+            bool has_comment = is_valid_position(comment_position);
+            if (has_comment)
+            {
+                out_file << out << '\n';
+                continue;
+            }
 
             std::size_t e_begin_position = line.find("<?e");
             std::size_t e_end_position = line.find("?>");
-            
 
+            bool has_begin = is_valid_position(e_begin_position);
+            bool has_end = is_valid_position(e_end_position);
+            if (has_begin and not has_end)
+            {
+                std::cerr << "Syntax Error " << s.filename() << " " << line_position << ":" << e_begin_position << " missing ?>" << '\n';
+                out_file  << "Syntax Error " << line_position << ":" << e_begin_position << " missing ?>" << '\n';
+                continue;
+            }
+
+            std::size_t include_position = has_begin ? line.find("include", e_begin_position) : std::string::npos;
+            bool has_begin_and_include = has_begin and is_valid_position(include_position);
+
+            if (has_begin_and_include)
+            {
+                std::size_t arg_begin_position = line.find("(\"", include_position);
+                std::size_t arg_end_position = line.find("\")", include_position);
+                bool has_arg = is_valid_position(arg_begin_position) and is_valid_position(arg_end_position);
+
+                if (not has_arg)
+                {
+                    std::cerr << "Error " << s.filename() << " " << line_position << ":" << e_begin_position << " include(\"\") missing argument" << '\n';
+                    out_file  << "Error " << line_position << ":" << e_begin_position << " include(\"\") missing argument" << '\n';
+                    continue;
+                }
+
+            }
 
             out_file << out << '\n';
         }
@@ -102,7 +137,5 @@ int main(int argc, char **argv)
         source_file.close();
     }
 
-
     return EXIT_SUCCESS;
 }
-
